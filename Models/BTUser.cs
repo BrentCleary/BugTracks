285e471d25376937e6bfda8e51b3c2b9dec230ae
Brent Cleary <brent.cleary@gmail.com>
2024-02-08 05:56:43 -0800

    Add project files.

---
+++ Models/BTUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTracks.Models
+{
+    public class BTUser : IdentityUser
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+        [NotMapped]
+        public string FullName 
+            { get
+                {
+                    return { FirstName + " " + LastName};
+                } 
+            }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        [NotMapped]
+        public string ConfirmPassword { get; set; }
+    }
+}
