30e1253733e8984c1c099c6116032a6ebc746eb6
Brent Cleary <brent.cleary@gmail.com>
2024-02-09 19:23:21 -0800

    Model Created for BugTracks BTUser

--- Models/BTUser.cs
+++ Models/BTUser.cs
@@ -8,7 +8,13 @@ namespace BugTracks.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         
         [NotMapped]
