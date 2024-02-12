1535cded782eede62e11ff6602177ed0d8971756
Brent Cleary <brent.cleary@gmail.com>
2024-02-12 06:55:22 -0800

    TicketComment Model Completed

---
+++ Models/TicketComment.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+
+namespace BugTracks.Models
+{
+    public class TicketComment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Member Comment")]
+        public string Comment { get; set; }
+
+        [DisplayName("Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+    
+        // Navigation Properties
+        public virtual Ticket Ticket { get; set; }
+
+        public virtual BTUser User { get; set; }
+    }
+}
