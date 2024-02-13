d0341213f7a84d9ca3e3d1d18a41122a6ccb0993
Brent Cleary <brent.cleary@gmail.com>
2024-02-13 07:05:32 -0800

    Many To Many Projects and BTUsers created for Join Table

--- Models/BTUser.cs
+++ Models/BTUser.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -21,7 +22,25 @@ namespace BugTracks.Models
         [Display(Name = "Full Name")]
         public string FullName { get { return $"{FirstName} {LastName}"; } }
 
+        // Image Properties
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+
+        [DisplayName("Avatar")]
+        public string AvatarFileName { get; set; }
+
+        public byte[] AvatarFileData { get; set; }
+        
+        [Display(Name = "File Extension")]
+        public string AvatarContentType { get; set; }
+
+        // Nav Ids
+        public int? CompanyId { get; set; }
 
 
+        // Navigation Properties
+        public virtual Company Company { get; set; }
+        public virtual ICollection<Project> Projects { get; set; }
     }
 }
