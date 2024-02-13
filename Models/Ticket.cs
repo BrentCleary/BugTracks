b6b9c93628509067e510c6c7193f8821b688e829
Brent Cleary <brent.cleary@gmail.com>
2024-02-13 05:46:20 -0800

    Ticket Model Started

---
+++ Models/Ticket.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BugTracks.Models
+{
+    public class Ticket
+    {
+        // Primary Key
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Created")]
+        public DateTimeOffset Created { get; set; }
+    
+        
+    
+    }
+}
