2b81a1e025651250202ecf9a1ce9fe412d488d07
Brent Cleary <brent.cleary@gmail.com>
2024-02-13 06:43:30 -0800

    Project and Project Priority Tickets Created

---
+++ Models/ProjectPriority.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTracks.Models
+{
+    public class ProjectPriority
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Priority Name")]
+        public string Name { get; set; }
+    
+    }
+}
