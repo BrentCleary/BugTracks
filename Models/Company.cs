99737cfa3457d5a384b193c35e937294f9aa1e02
Brent Cleary <brent.cleary@gmail.com>
2024-02-13 06:54:12 -0800

    Company Model Completed

---
+++ Models/Company.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace BugTracks.Models
+{
+    public class Company
+    {
+        // Primary Key
+        public int Id { get; set; }
+
+        [DisplayName("Company Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Company Description")]
+        public string Description { get; set; }
+
+
+        //Navivation Properties
+        public virtual ICollection<BTUser> Members { get; set; } 
+        public virtual ICollection<Project> Projects { get; set; }
+
+    }
+}
