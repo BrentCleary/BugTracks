da170cc66c615805998a0347d6c6c19f37a81256
Brent Cleary <brent.cleary@gmail.com>
2024-02-17 15:11:34 -0800

    ICompanyInfoService Completed

---
+++ Services/Interfaces/IBTCompanyInfoService.cs
@@ -0,0 +1,16 @@
+﻿using BugTracks.Models;
+
+namespace BugTracks.Services.Interfaces
+{
+    public interface IBTCompanyInfoService
+    {
+        public Task<Company> GetCompanyInfoByIdAsync(int? companyId);
+
+        public Task<List<BTUser>> GetAllMembersAsync(int companyId);
+    
+        public Task<List<Project>> GetAllProjectsAsync(int companyId);
+    
+        public Task<List<Ticket>> GetAllTicketsAsync(int companyId);
+
+    }
+}
