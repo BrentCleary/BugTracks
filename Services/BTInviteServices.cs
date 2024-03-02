b2ed4eb283d3b6c6f69fb36927fc9c5e8a4d3b8d
Brent Cleary <brent.cleary@gmail.com>
2024-03-02 10:25:46 -0800

    AcceptInviteAsync Completed

--- Services/BTInviteServices.cs
+++ Services/BTInviteServices.cs
@@ -1,15 +1,43 @@
-﻿using BugTracks.Models;
+﻿using BugTracks.Data;
+using BugTracks.Models;
 using BugTracks.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace BugTracks.Services
 
 {
     public class BTInviteServices : IBTInviteService
     {
-        public Task<bool> AcceptInviteAsync(Guid? token, string userId, int companyId)
+        private readonly ApplicationDbContext _context;
+
+        public BTInviteServices(ApplicationDbContext contex)
         {
-            throw new NotImplementedException();
+            _context = contex;
         }
+
+        public async Task<bool> AcceptInviteAsync(Guid? token, string userId, int companyId)
+        {
+            Invite invite = await _context.Invites.FirstOrDefaultAsync(i=>i.CompanyToken == token);
+
+            if(invite == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                invite.IsValid = false;
+                invite.InviteeId = userId;
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
 
         public Task AddNewInviteAsync(Invite invite)
         {
