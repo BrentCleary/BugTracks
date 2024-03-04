310acd4f493a51d4a2d576cd36d3489aba66a90e
Brent Cleary <brent.cleary@gmail.com>
2024-03-04 07:41:13 -0800

    GenerateClaimsAsync Completed

---
+++ Services/Factories/BTUserClaimsPrincipalFactory.cs
@@ -0,0 +1,26 @@
+﻿using BugTracks.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+
+namespace BugTracks.Services.Factories
+{
+    public class BTUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<BTUser, IdentityRole>
+    { 
+        public BTUserClaimsPrincipalFactory(UserManager<BTUser> userManager,
+                                            RoleManager<IdentityRole> roleManager,
+                                            IOptions<IdentityOptions> optionsAccessor)
+        : base(userManager, roleManager, optionsAccessor)
+        {
+
+        }
+
+        protected override async Task<ClaimsIdentity> GenerateClaimsAsync(BTUser user)
+        {
+            ClaimsIdentity identity = await base.GenerateClaimsAsync(user);
+            identity.AddClaim(new Claim("CompanyId", user.CompanyId.ToString()));
+
+            return identity;
+        }
+    }
+}
