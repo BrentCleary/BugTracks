24fb55eaf7e4234989e6bf2374be0171fa328665
Brent Cleary <brent.cleary@gmail.com>
2024-03-04 08:09:28 -0800

    IdentityExtensions Created

---
+++ Extensions/IdentityExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Principal;
+
+namespace BugTracks.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static int? GetCompanyId(this IIdentity identity)
+        {
+
+        }
+    }
+}
--- Services/Factories/BTUserClaimsPrincipalFactory.cs
+++ Services/Factories/BTUserClaimsPrincipalFactory.cs
@@ -22,5 +22,7 @@ namespace BugTracks.Services.Factories
 
             return identity;
         }
+
+
     }
 }
