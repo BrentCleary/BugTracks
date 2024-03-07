683cdb4db628ce663f8ce9c4036b888cf6160101
Brent Cleary <brent.cleary@gmail.com>
2024-03-07 08:03:40 -0800

    ManageUserRoles View Created

---
+++ Controllers/UserRolesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BugTracks.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Models/ViewModels/ManageUserRolesViewModel.cs
+++ Models/ViewModels/ManageUserRolesViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace BugTracks.Models.ViewModels
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BugTracks.Models.ViewModels
 {
     public class ManageUserRolesViewModel
     {
+        public BTUser BTUser { get; set; }
+        public MultiSelectList Roles { get; set; }
+        public List<String> SelectedRoles { get; set; }
+
     }
 }
