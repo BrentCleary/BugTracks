4dca54589ff515ee3dbf6eb291b6bdfebc0e3837
Brent Cleary <brent.cleary@gmail.com>
2024-03-10 20:13:33 -0700

    Details Page Created

--- Services/BTProjectService.cs
+++ Services/BTProjectService.cs
@@ -213,11 +213,27 @@ namespace BugTracks.Services
 
         public async Task<Project> GetProjectByIdAsync(int projectId, int companyId)
         {
+            //Project project = await _context.Projects
+            //                                .Include(p => p.Tickets)
+            //                                .Include(p => p.Members)
+            //                                .Include(p => p.ProjectPriority)
+            //                                .FirstOrDefaultAsync(p => p.Id == projectId && p.CompanyId == companyId);
+
             Project project = await _context.Projects
                                             .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketPriority)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketStatus)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.TicketType)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.DeveloperUser)
+                                            .Include(p => p.Tickets)
+                                                .ThenInclude(t => t.OwnerUser)
                                             .Include(p => p.Members)
                                             .Include(p => p.ProjectPriority)
                                             .FirstOrDefaultAsync(p => p.Id == projectId && p.CompanyId == companyId);
+
 
             return project;
         }
