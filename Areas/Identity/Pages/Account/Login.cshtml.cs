7e2b093a8df8a5213be7492fb6512e40875eb409
Brent Cleary <brent.cleary@gmail.com>
2024-03-24 12:50:37 -0700

    Dashboard Bottom Row Formating Complete

--- Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -104,7 +104,7 @@ namespace BugTracks.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Home/Dashboard");
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
