dbbf3bf6a014f66b5926edca230ac9e99f0a0da1
Brent Cleary <brent.cleary@gmail.com>
2024-03-24 12:24:10 -0700

    Dashboard View Users, Tickets and Projects Added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,8 @@
+using BugTracks.Extensions;
 using BugTracks.Models;
+using BugTracks.Models.ViewModels;
+using BugTracks.Services;
+using BugTracks.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +11,31 @@ namespace BugTracks.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBTCompanyInfoService _companyInfoService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, 
+                              IBTCompanyInfoService companyInfoService)
         {
             _logger = logger;
+            _companyInfoService = companyInfoService;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            DashboardViewModel model = new DashboardViewModel();
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            model.Company = await _companyInfoService.GetCompanyInfoByIdAsync(companyId);
+            model.Projects = (await _companyInfoService.GetAllProjectsAsync(companyId)).Where(p => p.Archived == false).ToList();
+            model.Tickets = model.Projects.SelectMany(p => p.Tickets).Where(t=>t.Archived == false).ToList();
+            model.Members = model.Company.Members.ToList();
+
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BugTracks.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public Company Company { get; set; }
+        public List<Project> Projects { get; set; }
+
+        public List<Ticket> Tickets { get; set; }
+
+        public List<BTUser> Members { get; set; }
+    }
+}
